395e8fe89b9c563eb8eb6187a43b82d003cb82ba
Sean Narron <seannarron9@gmail.com>
2022-09-22 16:44:12 +0900

    Made a simple login UI.

---
+++ client/src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Home() {
+
+    
+
+
+    return (
+        <div className="">
+            <div className="login">
+                <h2>Start Chatting!</h2>
+                <form action="">
+                    <label htmlFor="">Name</label>
+                    <input type="text" /><br />
+                    <label htmlFor="">Room</label>
+                    <input type="text" /><br />
+                    <button>Join</button>
+                </form>
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
