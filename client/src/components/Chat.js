fe87ada94fa40e3c5a08635181778472f0038018
Sean Narron <seannarron9@gmail.com>
2022-09-26 13:12:54 +0900

    Created a chat page.

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 function Chat({socket, userName, room}) {
@@ -9,23 +9,53 @@ function Chat({socket, userName, room}) {
 
     const sendMessage = () => {
 
-        if(currentMessage != null) {
+        if(currentMessage != "") {
             const messageData = {
                 author: userName,
                 room: room,
-                message: currentMessage
+                message: currentMessage,
+                time: new Date(Date.now()).getHours() +
+                ":" +
+                new Date(Date.now()).getMinutes()
             }
+
+            //send messae to data
             socket.emit("send_message", messageData)
+
             setMessageList((list) => [...list, messageData])
             setCurrentMessage('')
         }
     }
 
+
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            setMessageList((list) => [...list, data])
+        })
+    }, [socket])
+
     return (
         <div>
             <div className="chat">
                 <div className="chat-main">
-                    messages here
+                    {
+                        messageList.map(
+                            messages => (
+                                <div className="mesages" id={userName === messages.author ? "you" : "other"}>
+                                    <tbody>
+                                        <tr>
+                                            <td>{messages.message}</td>
+                                        </tr>
+                                        <tr>
+                                            <h6>{messages.author} - {messages.time}</h6>
+                                        </tr>
+                                    </tbody>
+                                </div>
+                                
+                                
+                            )
+                        )
+                    }
                 </div>
             </div>
             <div className="chat-send">
